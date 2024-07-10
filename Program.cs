81acbcaf29c002ddc5f5a702f184cbbe2605c477
TrongNghiakt <nguyentrongnghia.kt82@gmail.com>
2024-07-10 16:47:41 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nhom_6___SCC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"{num1} + {num2} = {addTwoNumber(num1,num2)}");
+
+
+            
+
+
+        }
+        static double addTwoNumber(double a, double b)
+            {
+                return a + b;
+            }
+
+        // Substract
+
+        // Divide
+
+        // Multiple
+
+
+
+
+
+    }
+}

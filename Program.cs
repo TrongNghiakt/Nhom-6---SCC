43285758a3b25d9371c01861123884871cf54b7d
TrongNghiakt <nguyentrongnghia.kt82@gmail.com>
2024-07-12 22:56:48 +0700

    Adding complex

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Nhom_6___SCC
 {
@@ -36,8 +37,29 @@ namespace Nhom_6___SCC
                 Console.WriteLine("Không thể chia được.");
             }
 
+            Console.Write("Enter the first real complex number: ");
+            double real1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the first imaginary complex number: ");
+            double imag1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the second real complex number: ");
+            double real2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the second imaginary complex number: ");
+            double imag2 = Convert.ToDouble(Console.ReadLine());
+
+            ComplexNumber complex1 = new ComplexNumber(real1, imag1);
+            ComplexNumber complex2 = new ComplexNumber(real2, imag2);
+
+            ComplexNumber complexSum = complex1 + complex2;
+
+            Console.WriteLine($"({complex1}) + ({complex2}) = {complexSum}");
+
+
+
+
 
             Console.ReadKey();
+
 
 
 
@@ -72,7 +94,28 @@ namespace Nhom_6___SCC
             return a * b;
         }
 
+        struct ComplexNumber
+        {
+            public double Real { get; }
+            public double Imaginary { get; }
 
+            public ComplexNumber(double real, double imaginary)
+            {
+                Real = real;
+                Imaginary = imaginary;
+            }
+
+            // Phương thức tính tổng hai số phức
+            public static ComplexNumber operator +(ComplexNumber c1, ComplexNumber c2)
+            {
+                return new ComplexNumber(c1.Real + c2.Real, c1.Imaginary + c2.Imaginary);
+            }
+
+            public override string ToString()
+            {
+                return $"{Real} + {Imaginary}i";
+            }
+        }
 
 
     }
